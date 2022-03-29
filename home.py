6181feb48973d0ed492c009d2dcd7e6230f20da1
Vincent Pham <vincent.t.pham@sjsu.edu>
2022-03-28 17:22:19 -0700

    Modified variables to be global

--- home.py
+++ home.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
-myapp_obj = Flask(__name__)
+myapp = Flask(__name__)
+name = "Lisa"
+city_names = ['Paris', 'London', 'Rome', 'Tahiti']
 
-@myapp_obj.route("/")
+@myapp.route("")
 def home():
-	name = "Lisa"
-	city_names = ['Paris', 'London', 'Rome', 'Tahiti']
 	return render_template('htmlf.html', name = name, lists = city_names)
 
-myapp_obj.run()
+#myapp.run()
 
