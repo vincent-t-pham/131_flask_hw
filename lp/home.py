ea7e5f8977939d5bc62901d80fcd13024d5737e5
Vincent Pham <vincent.t.pham@sjsu.edu>
2022-03-28 17:07:23 -0700

    Home.py and templates dir

---
+++ lp/home.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+myapp_obj = Flask(__name__)
+
+@myapp_obj.route("/")
+def home():
+	name = "Lisa"
+	city_names = ['Paris', 'London', 'Rome', 'Tahiti']
+	return render_template("htmlf.html", name=name, lists=city_names
+)
+
+myapp_obj.run()
+
