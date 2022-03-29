3ba0881072fff77e5de15f9b2997e08dc5f8e092
Vincent Pham <vincent.t.pham@sjsu.edu>
2022-03-28 17:26:57 -0700

    Modified flask object name

--- home.py
+++ home.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
-myapp = Flask(__name__)
+myobj = Flask(__name__)
 name = "Lisa"
 city_names = ['Paris', 'London', 'Rome', 'Tahiti']
 
-@myapp.route("")
+@myobj.route("")
 def home():
 	return render_template('htmlf.html', name = name, lists = city_names)
 
-#myapp.run()
+#myobj.run()
 
